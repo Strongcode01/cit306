3222ce79ca59d96d0590a9f07fad1e6eb323e52d
STRONGCODE <emmanueludoh360@gmail.com>
2025-10-12 18:03:18 +0100

    none

--- CIT306_GP14/urls.py
+++ CIT306_GP14/urls.py
@@ -26,7 +26,5 @@ urlpatterns = [
     path('team/', include('team.urls', namespace='team')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
